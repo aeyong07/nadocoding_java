44d8d771bfae428925d1797005f748a5b834f7dd
kimju0714 <rlaehdehd2001@gmail.com>
2023-03-08 12:14:20 +0900

    섹션2. 자료형과 변수 - 퀴즈까지 완료

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNum = "누리02";
+        int remainTime = 3;
+        double remainDistance = 1.2;
+
+        System.out.println(busNum + "번 버스");
+        System.out.println("약 " + remainTime + "분 후 도착");
+        System.out.println("남은 거리 " + remainDistance + "km");
+    }
+}
